5b8888605e890edc3d9fd617ecbade099109d025
zhangys <zhangys@dist.com.cn>
2020-09-27 17:22:27 +0800

    feat:增加网关模块

---
+++ atits-spring-cloud-gateway/src/main/java/com/atits/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.atits.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @description:
+ * @author: zhangys
+ * @create: 2020-09-27 10:06
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class);
+    }
+
+}
